b744d13fef29cc85686ffe7e849616173efc4ea6
lgoldbach <leander.goldbach@gmail.com>
2023-08-17 14:33:49 +0200

    adjust scripts for alternative alphabet folding

--- scripts/build_gp_map.py
+++ scripts/build_gp_map.py
@@ -4,6 +4,7 @@ import argparse
 
 from rna_folding.nussinov import BasePairMatrixNussinov
 from rna_folding.utils import bp_to_dotbracket
+from rna_folding.base_pairing import BasePairing
 
 
 if __name__ ==  "__main__":
@@ -16,14 +17,19 @@ if __name__ ==  "__main__":
                              "of max - s, where s is an integer. Without the flag, only one structure "
                              "is computed.")
     parser.add_argument("-z", "--structures_max", required=False, type=int, help="Limit on how many suboptimal structures to generate")
+    parser.add_argument("-p", "--base_pairing", required=False, type=int, default=-1, help="Which base-pairing to choose. I.e. from the base-pairing simple graphs, which one to pick "
+                        "e.g. for 4 bases there are 11 possible base-pairings, so possible input is any number between 1 and 11, If given -1 then it uses canonical base-pairing and AUGC bases")
+    parser.add_argument("-b", "--bases", required=False, type=str, default="AUGC", help="Which bases do the genotypes contain, e.g. 'AUGC' for canonical RNA")
+
 
     args = parser.parse_args()
 
+    pairing = BasePairing(bases=args.bases, id=args.base_pairing)
     phenotypes = {}
     with open(args.file, "r") as file_in:
         for i, sequence in enumerate(file_in):
             seq = sequence.strip()
-            P = BasePairMatrixNussinov(n=len(seq))
+            P = BasePairMatrixNussinov(n=len(seq), base_pairing=pairing)
             P.fill_matrix(seq=seq, min_loop_size=args.min_loop_size)
             strucs = P.traceback_subopt(seq=seq, d=args.suboptimal, structures_max=args.structures_max)
             for s in strucs:
--- scripts/job_scripts/rna12_gpmap_arrayjob.sh
+++ scripts/job_scripts/rna12_gpmap_arrayjob.sh
@@ -2,17 +2,17 @@
 
 #SBATCH --job-name=RNA12
 #SBATCH --nodes=1
-#SBATCH --array=1-49
+#SBATCH --array=1-50
 #SBATCH --time=02-00:00:00
-#SBATCH --output=/home/lgold/rna_folding/data/RNA12/output/output-%A_%a.out
+#SBATCH --output=/home/lgold/rna_folding/data/RNA12/alt_alphabets/four_letters/graph4_1
 #SBATCH --cpus-per-task=1
 
 SCRIPT_DIR="/home/lgold/rna_folding/scripts"
-INPUT_DIR="/home/lgold/rna_folding/data/RNA12/input"
-OUTPUT_DIR="/home/lgold/rna_folding/data/RNA12/output"
+INPUT_DIR="/home/lgold/rna_folding/data/RNA12/alt_alphabets/four_letters/genotypes"
+OUTPUT_DIR="/home/lgold/rna_folding/data/RNA12/alt_alphabets/four_letters/graph4_1"
 
 echo START: $(date '+%d/%m/%Y %H:%M:%S')
 
-srun python $SCRIPT_DIR/build_gp_map.py -f $INPUT_DIR/RNA12_gt.txt.${SLURM_ARRAY_TASK_ID} -o $OUTPUT_DIR/RNA12_ph.txt.${SLURM_ARRAY_TASK_ID} -m 3 -s 0
+srun python $SCRIPT_DIR/build_gp_map.py -f $INPUT_DIR/RNA12_gt${SLURM_ARRAY_TASK_ID}.txt -o $OUTPUT_DIR/RNA12_ph${SLURM_ARRAY_TASK_ID}.txt -m 3 -s 0 -b JKLM -p 1
 
 echo END: $(date '+%d/%m/%Y %H:%M:%S')
