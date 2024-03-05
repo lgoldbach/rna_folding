a33c1cfebec143cad4ccba10fb093f27843657f1
lgoldbach <leander.goldbach@gmail.com>
2024-03-05 10:27:58 +0100

    minor fixes

--- scripts/plotting/plot_phenotype_robustness_distribution.py
+++ scripts/plotting/plot_phenotype_robustness_distribution.py
@@ -8,8 +8,14 @@ from rna_folding.utils import load_phenotype_and_metric_from_file
 
 if __name__ ==  "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--files", help="Files containing phenotype distribution and robustness "
-                        "file", required=True, nargs="+")
+    parser.add_argument("-f", "--file", help="File with phenotype distribution and robustness "
+                        "file", required=True)
+    parser.add_argument("-r", "--ref_file", help="File with reference phenotype distribution and robustness "
+                        "file", required=True)
+    parser.add_argument("-b", "--bp_rule", help="Name of the bp_rule",
+                        required=True)
+    parser.add_argument("-i", "--ref_bp_rule", help="Name of the ref bp_rule",
+                        required=True)
     parser.add_argument("-o", "--output", help="File output for plot",
                         required=True)
 
@@ -17,9 +23,9 @@ if __name__ ==  "__main__":
 
     fig, (ax1, ax2, ax3)  = plt.subplots(nrows=3, figsize=(4, 5))
 
-    for file_name in args.files:
+    for bp_rule, file_name in zip([args.bp_rule, args.ref_bp_rule], [args.file, args.ref_file]):
         data = np.loadtxt(file_name, delimiter=' ')
-        ax1.hist(data[:, 0], alpha=0.5)
+        ax1.hist(data[:, 0], alpha=0.5, label=bp_rule)
         ax2.hist(np.log10(data[:, 1]), alpha=0.5)
         ratio = data[:, 0]/data[:, 1]
         ratio = np.log10(ratio[ratio!=0])
@@ -29,8 +35,9 @@ if __name__ ==  "__main__":
     ax2.set_xlabel("Phenotype frequency (log10)")
     ax3.set_xlabel("Phenotype robustness/Phenotype frequency (log10)")
     for ax in (ax1, ax2, ax3):
-        ax.set_ylabel("Count")
+        ax.set_ylabel("Phenotype count")
 
+    ax1.legend()
     plt.tight_layout()
     plt.savefig(args.output, format="pdf")
 
