8ae097663e45f641ca3635bf70d7d69251c0c3e7
lgoldbach <leander.goldbach@gmail.com>
2024-05-31 15:08:01 +0200

    plotting script for neutral path length dist. comapred to ref

---
+++ scripts/plotting/plot_joint_neutral_path_length_dist.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import argparse
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+if __name__ ==  "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--lengths", help="Path to neutral path length "
+                        "distribution files", required=True, nargs='+')
+    parser.add_argument("-r", "--ref_lengths", help="Reference neutral path "
+                        "lengths distribution file ", required=True)
+    parser.add_argument("-o", "--output", help="Output file name "
+                        "(should end in .pdf)", required=True)
+    
+    args = parser.parse_args()
+    fig, ax = plt.subplots()
+
+    for i, file in enumerate(args.lengths):
+        data = np.loadtxt(file, delimiter=" ", dtype=int).T
+        ax.scatter(data[0], data[1], color="blue")
+
+    # plot reference data
+    data = np.loadtxt(args.ref_lengths, delimiter=" ", dtype=int).T
+    ax.plot(data[0], data[1], color="black", marker=".")
+    
+
+    ax.set_xlabel("Count")
+    ax.set_ylabel("Neutral path length")
+        
+    plt.tight_layout()
+    plt.savefig(args.output, format="pdf", dpi=30)
